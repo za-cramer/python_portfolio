2dc2703e330eb751aac78ee9e9be6f8767f78f3f
Zach Cramer <121065404+za-cramer@users.noreply.github.com>
2023-08-16 10:53:15 -0600

    Update AS new faculty 21-23.py
    add email and make FTE for appt (not total of all appts)

--- AS new faculty 21-23.py
+++ AS new faculty 21-23.py
@@ -21,28 +21,18 @@ pers = pd.read_sas(sas_file_path, encoding='latin1')
 pers = pers[['EID','CUHireDate']]
 
 desired_codes = ['1100', '1101', '1102', '1103','1104', '1105', '1106']
-columns = (['EID','Year','EmpFirstName', 'EmpLastName','JobCode','JobTitle','DeptName', 
+columns = (['EID','Year','EmpFirstName', 'EmpLastName','EmpEmail','JobCode','JobTitle','DeptName', 
             'College', 'ASDiv', 'Order','Time'])
 
 # narrow dataframe # 
 filtered_df = df[(df['College'] == 'AS') & (df['JobCode'].isin(desired_codes))][columns]
-filtered_df
-
-df = df[columns] # for all appts for FTE #
 
 # Add CU Hire Date #
 merged_df = pd.merge(filtered_df, pers, on = 'EID', how = 'left')
 
 merged_df['Hire Year'] = merged_df['CUHireDate'].dt.year
 
-# Calculate FTE by Year #
-FTE = df.groupby(['EID', 'Year'])['Time'].sum().reset_index()
-FTE['FTE'] = FTE['Time']
-FTE = FTE.drop('Time',axis = 1)
-
-merged_df_fte = pd.merge(merged_df, FTE, on = ['EID','Year'], how = 'left')
-
-df = merged_df_fte[merged_df_fte['Hire Year'] >= 2021]
+df = merged_df[merged_df['Hire Year'] >= 2021]
 df = df.sort_values(['EID', 'Year','Order'], ascending=[True, False, True])
 
 dup_criteria = ['EID','JobCode']
@@ -54,10 +44,10 @@ df_final = df.drop_duplicates(subset = dup_criteria)
 
 df_final['ASDiv'] = df_final['ASDiv'].map(as_map)
 
-columns = (['EID','EmpFirstName','EmpLastName','JobCode','JobTitle','DeptName',
-            'ASDiv','CUHireDate','Hire Year','FTE'])
+columns = (['EID','EmpFirstName','EmpLastName','EmpEmail','JobCode','JobTitle',
+            'DeptName','ASDiv','CUHireDate','Hire Year','Time'])
 
 df_final = df_final[columns]
-df_final.info()
+df_final['FTE'] = df_final['Time']
 
 df_final.to_csv('M:\\Decrypt\\as_newfaculty_2123.csv', index = False)
